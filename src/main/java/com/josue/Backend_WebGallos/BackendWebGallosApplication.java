afef62501477e8ab09e360a9592b5a7caa472c99
Josue <josuegery@gmail.com>
2025-06-10 15:50:04 -0500

    Proyecto base Backend-WebGallos generado por Spring Initializr

---
+++ src/main/java/com/josue/Backend_WebGallos/BackendWebGallosApplication.java
@@ -0,0 +1,13 @@
+package com.josue.Backend_WebGallos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWebGallosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWebGallosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josue/Backend_WebGallos/BackendWebGallosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josue.Backend_WebGallos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWebGallosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
